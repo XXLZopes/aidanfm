5ae78c78cf2eaa84d205eedae92d9cbcecdc5dac
XXLZopes <afordm99@gmail.com>
2022-07-19 19:49:28 -0700

    initial commit

---
+++ assets/js/circles.js
@@ -0,0 +1,62 @@
+const backgroundEl = document.querySelector('#background');
+const testEl = document.querySelector('#body')
+
+class Circle {
+    constructor(x, y, r, color, txt) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.color = color
+        this.txt = txt;
+    }
+    setPosition(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+//Declare circle
+let circle0 = new Circle('0', '0', '15', '#A3BECC', 'hi');
+let circle1 = new Circle('30', '150', '20', 'red', 'hi');
+let circle2 = new Circle('-1', '250', '27', 'red', 'hi');
+let circle3 = new Circle('30', '400', '15', 'red', 'hi');
+let circle4 = new Circle('-1', '-1', '33', 'red', 'hi');
+
+let circles = [circle0, circle1, circle2, circle3, circle4]
+
+function generateCircles (circleArray, parentElement, className) {
+    circleArray.forEach((circle, i) => {
+        let circleEl = document.createElement('div');
+    
+        //Circle START
+        circleEl.setAttribute('id', `circle${i}`);
+        circleEl.setAttribute('class', className);
+    
+        if(circle.x < 0) {
+            circleEl.style.right = (circle.x * -1) - 1 + 'vw';
+        } else {
+            circleEl.style.left = circle.x + 'vw';
+        }
+    
+        if(circle.y < 0) {
+            circleEl.style.bottom = (circle.y * -1) - 1 + 'px';
+        } else {
+            circleEl.style.top = circle.y + 'px';
+        }
+
+        circleEl.style.height = window.innerWidth < 700 ? circle.r + 'vw' : circle.r * .6 + 'vw';
+        circleEl.style.width = window.innerWidth < 700 ? circle.r + 'vw' : circle.r * .6 + 'vw';
+
+        window.addEventListener('resize', function() {
+            circleEl.style.height = window.innerWidth < 700 ? circle.r + 'vw' : circle.r * .6 + 'vw';
+            circleEl.style.width = window.innerWidth < 700 ? circle.r + 'vw' : circle.r * .6 + 'vw';
+        }, false)
+        
+        //Circle END
+    
+        parentElement.appendChild(circleEl);
+    });
+
+}
+
+generateCircles(circles, backgroundEl, 'circle');
+generateCircles(circles, testEl, 'circle1');
---
+++ assets/js/scroll.js
@@ -0,0 +1,46 @@
+const backgroundContainerEl = document.querySelector('#background-con');
+
+backgroundContainerEl.style.height = document.documentElement.clientHeight + 'px';
+backgroundContainerEl.style.width = '100.0vw'
+
+let scrollArray = [window.scrollY]
+let totalScroll = 0;
+
+let currentHeight = document.documentElement.clientHeight;
+
+function raiseCurtain(curtain) {
+    scrollArray.push(window.scrollY)
+    let changeInHeight = scrollArray[0] - scrollArray[1];
+    let curtainHeight = parseInt(curtain.style.height);
+    
+    curtain.style.height = curtainHeight + changeInHeight + 'px';
+    scrollArray.shift();
+
+    if (window.scrollY <= 0) {
+        scrollArray = [0];
+        curtain.style.height = document.documentElement.clientHeight + 'px';
+    }
+
+    if (window.scrollY >= window.innerHeight) {
+        curtain.style.height = 0;
+    }
+}
+
+function adjustHeight(curtain) {
+    let curtainHeight = parseInt(curtain.style.height);
+    let maxHeight = document.documentElement.clientHeight;
+    let heightDifference = maxHeight - currentHeight;
+    let adjustedHeight =  curtainHeight + heightDifference;
+    curtain.style.height = adjustedHeight + 'px'
+    console.log(heightDifference, maxHeight, currentHeight)
+
+
+}
+
+window.addEventListener("scroll", function () {
+    raiseCurtain(backgroundContainerEl);
+}, false);
+
+// window.addEventListener("resize", function () {
+//     adjustHeight(backgroundContainerEl)
+// }, false)
